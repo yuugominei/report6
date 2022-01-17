d553e44b62211fca102bc0cd4e8cf8c2d40ab762
yuugo minei <e215740@ie.u-ryukyu.ac.jp>
2022-01-18 03:26:26 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215740/Baseball.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215740/Baseball.java
@@ -1,10 +1,15 @@
 package jp.ac.uryukyu.ie.e215740;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Baseball{
     static String [][] strikezone = { { "・", "・", "・" }, { "・", "・", "・" }, { "・", "・", "・" } };
     int strikeCount = 0;
+    boolean strikeOut = false;
+    boolean homerun = false;
+    static boolean aim = false;
+    boolean gameSet = false;
     
     public void zone(){
         for(int a = 0; a < 3; a++){
@@ -24,9 +29,51 @@ public class Baseball{
             if(strikezone[lengthCourse][widthCourse] == "・"){
                 strikezone[lengthCourse][widthCourse] = "○";
             }
-        }
-        
+        }       
         System.out.println("ピッチャー構えます");
     }
+    public void batter(){
+        Scanner scanLength = new Scanner(System.in);
+        Scanner scanWidth = new Scanner(System.in);
+        aim = false;
+       
+        while(aim == false){
+            System.out.println("高さの入力");
+            int length = scanLength.nextInt();
 
+            System.out.println("横の入力");
+            int width = scanWidth.nextInt();
+
+            if(strikezone[length -1][width - 1] == "✕"){
+                System.out.println("そこじゃない!やり直し!");         
+            }
+            if (strikezone[length - 1][width - 1] == "・"){
+                strikezone[length - 1][width - 1] = "✕";
+                strikeCount += 1;
+                System.out.println("ストライク!");
+                         
+                if(strikeCount == 3){
+                    strikeOut = true;
+                    System.out.println("バッターアウト");
+                    gameSet = true;
+                    if(gameSet = true){
+                        break;
+                    }
+                }     
+            }
+            if (strikezone[length - 1][width - 1] == "○"){
+                homerun = true;
+                System.out.println("ホームラン!!");
+                gameSet = true;
+                if(gameSet = true){
+                    break;
+                }
+            }
+        }
+        
+        if (gameSet == true){
+            scanLength.close();
+            scanWidth.close();          
+        }
+    }
 }
