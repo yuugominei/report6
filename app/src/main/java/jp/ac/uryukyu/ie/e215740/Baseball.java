910410ec58104fa71dd6249a275a46212fff72f4
yuugo minei <e215740@ie.u-ryukyu.ac.jp>
2022-01-18 03:21:43 +0900

    1st commit

--- app/src/main/java/jp/ac/uryukyu/ie/e215740/Baseball.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215740/Baseball.java
@@ -1,7 +1,10 @@
 package jp.ac.uryukyu.ie.e215740;
 
+import java.util.Random;
+
 public class Baseball{
     static String [][] strikezone = { { "・", "・", "・" }, { "・", "・", "・" }, { "・", "・", "・" } };
+    int strikeCount = 0;
     
     public void zone(){
         for(int a = 0; a < 3; a++){
@@ -12,4 +15,18 @@ public class Baseball{
             System.out.println();
         }
     }
+    public void pitcher(){
+        
+        if(strikeCount < 3){
+            Random pitcher = new Random();
+            int lengthCourse = pitcher.nextInt(3);
+            int widthCourse = pitcher.nextInt(3);
+            if(strikezone[lengthCourse][widthCourse] == "・"){
+                strikezone[lengthCourse][widthCourse] = "○";
+            }
+        }
+        
+        System.out.println("ピッチャー構えます");
+    }
+
 }
